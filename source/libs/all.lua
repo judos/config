2336d3ed7ed71f267d14f3c7c82590efd14398c5
judos <judos@gmx.ch>
2016-11-27 21:36:21 +0100

    refactor basic-lua-extensions -> lua

--- source/libs/all.lua
+++ source/libs/all.lua
@@ -1,3 +1,6 @@
 require "libs.logging"
-require "libs.basic-lua-extensions"
+require "libs.lua.table"
+require "libs.lua.string"
+require "libs.lua.set"
+require "libs.lua.various"
 require "libs.find-raw-ingredients"
--- source/libs/prototypes/entity.lua
+++ source/libs/prototypes/entity.lua
@@ -1,13 +1,16 @@
 -- entitiesTable: table of entities from data.raw table
 -- propertyName: may use dots inside the name to access sub properties
 function changeEntitiesPropertyByFactor(entitiesTable, propertyName, factor, roundValues)
+	local entitiesName = {}
 	for _,data in pairs(entitiesTable) do
 		if data[propertyName] then
 			data[propertyName] = data[propertyName] * factor
 			if roundValues then data[propertyName] = round(data[propertyName]) end
-			info(data.name.."."..propertyName.." * "..tostring(factor).." -> "..tostring(data[propertyName]))
+			--info(data.name.."."..propertyName.." * "..tostring(factor).." -> "..tostring(data[propertyName]))
 		else
 			warn(propertyName.." does not exist for "..data.name)
 		end
+		table.insert(entitiesName, data.name)
 	end
+	info("Change "..propertyName.." * "..tostring(factor).." for: "..serpent.block(entitiesName))
 end
